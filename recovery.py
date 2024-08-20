d7f1d6981899a05e5fe756e8454dcc4edb947c0c
Alfio Puglisi <alfio.puglisi@gmail.com>
2024-08-20 16:58:23 +0200

    Recovered raid1 status

--- recovery.py
+++ recovery.py
@@ -2,24 +2,34 @@
 import itertools
 import functools
 import operator
+import numpy as np
 
 def read_data(prefix, page_size, page):
-    f = open(prefix+'.img', 'rb')
+    f = open('end'+prefix+'.img', 'rb')
     f.seek(page * page_size)
-    return f.read(page_size)
+    byt = f.read(page_size)
+    return np.frombuffer(byt, dtype=np.uint8)
 
 def guess(prefixes, raid5_size, page_size, page):
     for comb in itertools.combinations(prefixes, raid5_size):
+        print(comb)
         data = [read_data(prefix, page_size, page) for prefix in comb]
-        parity = functools.reduce(data[1:], operator.xor)
-        check = (data[0] == parity)
+        parity = functools.reduce(operator.xor, data[1:])
+        check = np.array_equal(data[0], parity)
+
+        #import code
+        #code.interact(local=dict(globals(), **locals()))
+
         if check:
-            return comb
+           print('Match')
 
 def main():
     page_size = 64 * 1024
-    prefixes = '0 1 2 3 4 5 6 7'.split()
+    prefixes = '1 2 3 4 5 6 7 8'.split()
     raid5_size = 4
-    page = 1000000
+    page = 16083
     guess(prefixes, raid5_size, page_size, page)
 
+
+if __name__ == '__main__':
+    main()
